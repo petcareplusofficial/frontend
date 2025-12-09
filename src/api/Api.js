65adc2522cb6a701d1fa226c1f43494713f230d8
petcareplusofficial <petcareplusofficial@gmail.com>
2025-12-09 06:50:52 +0000

    server config

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:4000/api/v1/petcare";
+const BASE_URL = "https://backend-5x9p.onrender.com/api/v1/petcare";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
